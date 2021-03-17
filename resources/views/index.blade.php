871583ec3ce08f7df8ff4a5c39a66f234ce9b229
Heibert Ocaña <heibertvts1997@gmail.com>
2021-03-17 14:10:36 -0500

    Inicio del proyecto con laravel 8

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.pages')
+
+@section('css')
+  <link rel="stylesheet" href="{{asset('src/css/page.structure.css')}}">    
+@endsection
+
+@section('section')
+<div class="container-xl">
+  <h1 class="text-center text-danger">Home</h1>
+
+</div>
+@endsection
+
+@section('js')
+    
+@endsection
---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,4 @@
+<header class="container-xl">
+  <img src="{{asset("src/img/logo-t1-x2.png")}}" alt="">
+  Menu
+</header>
